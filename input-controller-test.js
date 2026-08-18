652675a872fa23035478bcfa649ffbdba4fac887
Ilia <hikkikomori7@gmail.com>
2026-08-18 11:24:14 +0300

    feat: добавлены логи кастомных событий отправляемых к прикрепленному DOM элементу

--- input-controller-test.js
+++ input-controller-test.js
@@ -6,7 +6,7 @@ const detachButton = document.querySelector('.button--detach');
 const enableButton = document.querySelector('.button--enable');
 const disableButton = document.querySelector('.button--disable');
 const bindJumpButton = document.querySelector('.button--bind-jump');
-const info = document.querySelector('.info-container');
+const eventLogs = document.querySelector('.event-logs');
 
 const controller = new InputController();
 controller.bindActions({
@@ -50,6 +50,16 @@ detachButton.onclick = detachTarget;
 enableButton.onclick = enableController;
 disableButton.onclick = disableController;
 bindJumpButton.onclick = bindJump;
+
+function logCustomEvents(customEvent) {
+    const div = document.createElement('div');
+    const text = `${new Date().toLocaleTimeString()} : ${customEvent.detail} => ${customEvent.type}`;
+    div.textContent = text;
+    eventLogs.appendChild(div);
+}
+
+testTarget.addEventListener(controller.ACTION_ACTIVATED, logCustomEvents);
+testTarget.addEventListener(controller.ACTION_DEACTIVATED, logCustomEvents);
 
 let startTime;
 let xCoord = 0;
