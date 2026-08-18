bd1d27eccc60d7f51d993f6fe33b608af9335207
Ilia <hikkikomori7@gmail.com>
2026-08-18 15:22:48 +0300

    feat: добавлена первая версия класса KeyboardPlugin - плагина клавиатуры для InputController

---
+++ keyboard-plugin.js
@@ -0,0 +1,58 @@
+/**
+ * Класс плагина клавиатуры для работы с InputController
+ */
+class KeyboardPlugin extends InputPlugin {
+    type = 'keys';
+    #parentDocument;
+
+    /**
+     * Обработчик для события keydown
+     * @param {object} event - объект Keyboard Event
+     */
+    #onKeyDown(event) {
+        if (event.keyCode) {
+            this.press(event.keyCode);
+        }
+    }
+
+    /**
+     * Обработчик для события keyup
+     * @param {object} event - объект Keyboard Event
+     */
+    #onKeyUp(event) {
+        if (event.keyCode) {
+            this.release(event.keyCode);
+        }
+    }
+
+    /**
+     * Вешает слушатели событий на родительский документ переданного DOM элемента
+     * @param {object} target - DOM элемент 
+     */
+    attach(target) {
+        if (this.target) {
+            this.detach();
+        }
+
+        this.#parentDocument = target?.ownerDocument;
+        super.attach(target);
+
+        if (this.#parentDocument) {
+            this.#parentDocument.addEventListener('keydown', this.#onKeyDown);
+            this.#parentDocument.addEventListener('keyup', this.#onKeyUp);
+        }
+    }
+
+    /**
+     * Удаляет слушатели событий при наличии
+     */
+    detach() {
+        if (this.#parentDocument) {
+            this.#parentDocument.removeEventListener('keydown', this.#onKeyDown);
+            this.#parentDocument.removeEventListener('keyup', this.#onKeyUp);
+        }
+
+        this.#parentDocument = null;
+        super.detach();
+    }
+}
